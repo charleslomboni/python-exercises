f36a6d55fae85bec38a696b578a8fadf54e8499d
charleslomboni <charleslomboni@gmail.com>
2023-02-14 11:32:56 -0300

    Exercise 1

---
+++ createfile.py
@@ -0,0 +1,7 @@
+def write_file(text):
+    
+    f = open("simply.txt", "a", encoding="utf-8")
+    f.write(text)
+    f.close()
+
+write_file("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum")
